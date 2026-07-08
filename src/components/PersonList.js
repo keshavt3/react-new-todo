ddcebe3bce8ee998ea6113f3068ca6dd955d20bb
Training <training@ip-172-31-27-6.us-east-2.compute.internal>
2026-07-08 20:05:18 +0000

    fix api call

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://q76xnqzf38.execute-api.us-west-2.amazonaws.com/prod/get-person`)
+    axios.get(`https://hn9rgyzrdf.execute-api.us-west-2.amazonaws.com/keshav-prod`)
       .then(res => {
         const persons = res.data.body;
         this.setState({ persons });
